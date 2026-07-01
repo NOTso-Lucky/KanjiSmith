552753745a68e0102420aefe5a77b3b12d0f901a
NOTso-Lucky <harshit.y252005@gmail.com>
2026-07-01 20:27:27 +0530

    Add requirements.txt for deploy

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,22 +3,24 @@ from app.db.database import Base, engine
 import app.models
 from app.routers import words, auth, users, deck, flashcard, review, dashboard, settings
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
 app = FastAPI(
     title="KanjiSmith API",
     version="1.0.0"
 )
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        os.getenv("FRONTEND_URL", ""),
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(words.router)
 app.include_router(auth.router)
 app.include_router(users.router)
